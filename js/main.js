f04c2976be856d3899594a0766de33ca27b377c8
XMSMApi <93034430+XMSMApi@users.noreply.github.com>
2022-03-18 22:06:26 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
 window.onload = function getAnnouncement() {
-    var xhr = new XMLHttpRequest;
     var lbg = document.getElementById('lbg');
-    xhr.open("GET", "https://lolimstatic.ml/announcement.md");
-    xhr.send();
-        if (xhr.status = 200) {
-            var converter = new showdown.Converter(),
-                text      = xhr.ReponseText,
-                html      = converter.makeHtml(text);
-                lbg.innerHTML = html;
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", "https://lolimstatic.ml/announcement.md");
+    oReq.send();
+    oReq.onreadystatechange = function () {
+        if (oReq.readyState = 4) {
+            showdownjs(oReq.responseText);//console.log(oReq.responseText);
         } else {
-            lbg.innerHTML = "获取公告失败！";
+            lbg.innerHTML = "获取信息失败！";
         }
-    
+    }
 }
+function showdownjs(data) {
+    var converter = new showdown.Converter(),
+        html      = converter.makeHtml(data);
+        lbg.innerHTML = html;
+}
+
+fetch('https://api.github.com/repos/SkiMinoTeam/API/commits').then(
+		(response) => {
+			response.json().then((data) => {
+				const changeLogFrame=document.querySelector('#cms');
+				data.forEach(commit => {
+					const item=document.createElement('a');
+					item.classList.add('item');
+					item.href=commit.html_url;
+					item.setAttribute('data-sha',commit.sha.slice(0,7));
+					item.innerText=commit.commit.message;
+					changeLogFrame.appendChild(item);
+				});
+			});
+		}
+	);
