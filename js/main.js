27cd84ade1bc4ba6c488b01c60abdad3d92847ca
XMSMApi <93034430+XMSMApi@users.noreply.github.com>
2022-03-18 20:46:49 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-var converter = new showdown.Converter(),
-    text      = '# hello, markdown!',
-    html      = converter.makeHtml(text);
+window.onload = function getAnnouncement() {
+    var xhr = new XMLHttpRequest;
+    var lbg = document.getElementById('lbg');
+    xhr.open("GET", "https://lolimstatic.ml/announcement.md");
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.reponseText != undefined) {
+            var converter = new showdown.Converter(),
+                text      = xhr.reponseText,
+                html      = converter.makeHtml(text);
+                lbg.innerHTML = html;
+        } else {
+            lbg.innerHTML = "获取公告失败！";
+        }
+    }
+}
